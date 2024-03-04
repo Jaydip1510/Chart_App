0f3db2ce32e4b615e19afe028a9744071846b1f3
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-03-04 21:34:58 +0530

    headerfile

---
+++ client/src/components/layout/Header.jsx
@@ -0,0 +1,16 @@
+import { AppBar, Box } from '@mui/material'
+import React from 'react'
+
+const Header = () => {
+  return <>
+   
+   <Box sx={{flexGrow:1}} height={"4rem"}>
+    <AppBar position='static' sx={{
+        bgcolor:"#ea7070"
+    }}/> 
+   </Box>
+    
+  </>
+}
+
+export default Header
