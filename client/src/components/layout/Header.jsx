092ace2af6777454396de2a59eda4432e80ca484
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-03-05 09:52:33 +0530

    changes

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,13 +1,42 @@
-import { AppBar, Box } from '@mui/material'
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import { orange } from '../../constants/color';
+import {Menu as MenuIcon} from '@mui/icons-material';
 
 const Header = () => {
+    
+    const handleMobile = () => {
+      console.log('Moblie');
+    }
+
   return <>
    
    <Box sx={{flexGrow:1}} height={"4rem"}>
     <AppBar position='static' sx={{
-        bgcolor:"#ea7070"
-    }}/> 
+        bgcolor:orange
+    }}
+    >
+
+   <Toolbar>
+     <Typography variant='h6' sx={{
+       display:{xs:"none",sm:"block"},
+     }}>
+      Chattu
+     </Typography>
+     <Box sx={{
+            display:{xs:"block",sm:"none"},
+     }}
+     >
+      <IconButton color='inherit' onClick={handleMobile}>
+      <MenuIcon/>
+
+      </IconButton>
+     </Box>
+     <Box>Final</Box>
+
+   </Toolbar>
+
+     </AppBar> 
    </Box>
     
   </>
