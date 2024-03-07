0fc2c28682a76e557be5083102025511c9abee32
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-03-07 18:47:32 +0530

    groupfind

--- client/src/constants/SampleData.jsx
+++ client/src/constants/SampleData.jsx
@@ -21,12 +21,31 @@ export const sampleChats = [
 export const sampleUsers = [
     {
         avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
-        name: "John Doe  ",
+        name: "John Smith  ",
         _id: "1",
     },
     {
         avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
         name: "John Doe  ",
+        _id: "2",
+    }
+];
+
+
+
+export const sampleNotifications = [
+    {
+        sender: {
+            avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+            name: "John Doe  ",
+        },
+        _id: "1",
+    },
+    {
+        sender: {
+            avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+            name: "John Smith  ",
+        },
         _id: "2",
     }
 ];
