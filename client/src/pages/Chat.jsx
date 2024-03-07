b8a2cadb3ed26d1701b34fb33c525aa05d2b61e7
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-03-07 21:18:38 +0530

    msg

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { Fragment, useRef } from 'react'
 import AppLayout from '../components/layout/AppLayout'
-function Chat() {
+import { IconButton, Stack } from '@mui/material';
+import { grayColor } from '../constants/color';
+import { AttachFile as AttachFileIcon, Send as SendIcon } from '@mui/icons-material';
+import { InputBox } from '../components/styles/StyledComponents';
+const Chat = () => {
+
+  const containerRef = useRef(null);
   return (
-    <div>
-      Chat
-    </div>
+    <Fragment>
+      <Stack
+        ref={containerRef}
+        boxSizing={"border-box"}
+        padding={"1rem"}
+        spacing={"1rem"}
+        bgcolor={grayColor}
+        height={"90%"}
+        sx={{
+          overflow: "hidden",
+          overflow: "auto"
+        }}
+      >
+        {/* Messages Render */}
+      </Stack>
+       <form style={{
+           height:"10%",
+       }}
+       >
+
+       <Stack 
+       direction={"row"} 
+       height={"100%"} 
+       padding={"1rem"}
+       alignItems={"center"}
+       position={"relative"}
+       >
+          <IconButton>
+            <AttachFileIcon/>
+          </IconButton>
+ 
+       <InputBox placeholder='Type Message Here...'/>
+       <IconButton>
+         <SendIcon/>
+       </IconButton>
+
+
+       </Stack>
+       </form>
+
+    </Fragment>
   )
 }
 
